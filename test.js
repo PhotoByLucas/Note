1cc721be8cf29fca39556f7541735d3587178b89
Lucas <835934204@qq.com>
2019-10-11 15:40:03 +0800

    2019-10-11

--- test.js
+++ test.js
@@ -1,8 +1,21 @@
-function a() {
-	console.log(arguments)
-	console.log(arguments.length)
+const data = {
+	a: { b: { c: { d: 1, e: 2 } } }, f: 3, g: { k: 'nmlgb' }
+}
+let items = ['a.b.c.d', 'a.b.c.e', 'f', 'g.k', 'i.k']
+function test(data, item) {
+	if (item.length > 1) {
+		let temp = item.shift()
+		if (!data[temp]) return undefined
+		return test(data[temp], item)
+	}
+	else {
+		if (!data[item[0]]) return undefined
+		console.log(data[item[0]])
+		return data[item[0]]
+	}
 }
 
-a(1,32,3,3,3)
-console.log(b)
-var b =20
+items.forEach(item => {
+
+	test(data, item.split('.'))
+});
