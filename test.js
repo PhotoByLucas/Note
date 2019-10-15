8331ae83ee63eea870a4aeed0d036892e08fe343
Lucas <835934204@qq.com>
2019-10-15 10:55:07 +0800

    2019-10-15

--- test.js
+++ test.js
@@ -1,21 +1,19 @@
-const data = {
-	a: { b: { c: { d: 1, e: 2 } } }, f: 3, g: { k: 'nmlgb' }
-}
-let items = ['a.b.c.d', 'a.b.c.e', 'f', 'g.k', 'i.k']
-function test(data, item) {
-	if (item.length > 1) {
-		let temp = item.shift()
-		if (!data[temp]) return undefined
-		return test(data[temp], item)
+// demo2
+function test() {
+	console.log(foo); //foo(){...}
+	console.log(bar); //undefined
+
+	var foo = 'Hello';
+	console.log(foo); //'Hello'
+	var bar = function () {
+			return 'world';
 	}
-	else {
-		if (!data[item[0]]) return undefined
-		console.log(data[item[0]])
-		return data[item[0]]
+	
+	console.log(bar); //bar(){...}
+
+	function foo() {
+			return 'hello';
 	}
 }
 
-items.forEach(item => {
-
-	test(data, item.split('.'))
-});
+test();
