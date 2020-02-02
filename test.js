2d2ec537449282e606f3607acb3c0893fa6eeb2f
Lucas <835934204@qq.com>
2020-02-02 22:34:18 +0800

    2020-02-02

--- test.js
+++ test.js
@@ -1,17 +1,34 @@
-var trap = function(height=[]) {
-  let max=0
-  height.forEach((element)=>{
-    if(element>max){
-      max=element
+var trap = function(height = []) {
+  let water = 0;
+  for (let i = 1; i < height.length; i++) {
+    let maxLeft=height[i]
+    let maxRight=height[i]
+    // 左边
+    for (let j = i; j >= 0; j--) {
+      maxLeft=getMax(maxLeft,height[i])
     }
-  })
-  console.log(max)
-  for (let index = 0; index < height.length; index++) {
-    const element = height[index];
-    
+    // 右边
+    for (let j = i; j <height.length; j++) {
+      maxRight=getMax(maxRight,height[i])
+    }
+
+    console.log(maxLeft,maxRight,height[i])
+    water+=(getMin(maxLeft,maxRight)-height[i])
   }
-    
+  
+  function getMax(a,b){
+    return a>b?a:b
+  }
+
+  function getMin(a,b){
+    return a<b?a:b
+  }
+
+
+  
+  
+  console.log("water",water);
 };
-var a=[0,1,0,2,1,0,1,3,2,1,2,1]
+var a = [0,1,0,2,1];
 
 console.log(trap(a));
