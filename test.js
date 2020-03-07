79c265cc4655fc1f09f54449df47beba95de2a20
Lucas <835934204@qq.com>
2020-03-07 17:30:00 +0800

    2020-03-07

--- test.js
+++ test.js
@@ -1,39 +1,12 @@
-let inputN = 4;
-let tag = 1;
-let myNumber = new Array();
-for (let i = 0; i < inputN + 1; i++) {
-  // 新建一个n+1长度的初始化为0的数组
-  myNumber.push(0);
+let a = {
+  value: 1
 }
-
-while (tag) {
-  print(tag);
-  increase(inputN);
+function getValue(name, age) {
+  console.log(name)
+  console.log(age)
+  console.log(this.value)
 }
+// getValue.call(a, 'yck', '24')
+// getValue.apply(a, ['yck', '24'])
 
-// 增长函数 立刻执行
-// n 为+1的数组位
-function increase(n) {
-  // 最高一位为1时，说明已经超过n位
-  if (myNumber[0] !== 0) {
-    tag = false;
-  }
-
-  myNumber[n] += 1;
-  if (myNumber[n] === 10) {
-    // 进位
-    if (myNumber[inputN-tag] === 0&&inputN-n+1===tag) {
-      tag += 1;
-    }
-    increase(n - 1);
-    myNumber[n] = 0;
-  }
-}
-
-function print(tag) {
-  if (myNumber[0] !== 0) return;
-
-  let tempNum = myNumber.slice(inputN - tag + 1, inputN + 1);
-  // console.log(myNumber, tempNum, tag);
-  console.log(tempNum.join(""),tag);
-}
+(bind(a,'yck','24'))()
---
+++ 各种阅读学习笔记/面试-JS相关.md
@@ -0,0 +1,35 @@
+1. 变量提升 => 前端基础进阶 变量对象
+2. bind apply call
+
+    bind
+    ~~~
+    this.x = 9;    // 在浏览器中，this 指向全局的 "window" 对象
+    var module = {
+      x: 81,
+      getX: function() { return this.x; }
+    };
+
+    module.getX(); // 81
+
+    var retrieveX = module.getX;
+    retrieveX();   
+    // 返回 9 - 因为函数是在全局作用域中调用的
+
+    // 创建一个新函数，把 'this' 绑定到 module 对象
+    // 新手可能会将全局变量 x 与 module 的属性 x 混淆
+    var boundGetX = retrieveX.bind(module);
+    boundGetX(); // 81
+    ~~~
+    call apply
+    ~~~
+    let a = {
+      value: 1
+    }
+    function getValue(name, age) {
+      console.log(name)
+      console.log(age)
+      console.log(this.value)
+    }
+    getValue.call(a, 'yck', '24')
+    getValue.apply(a, ['yck', '24'])
+    ~~~
