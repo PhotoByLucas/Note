5e7fcf81f9f5a549cbc08235747429be83ba4f20
Lucas <835934204@qq.com>
2020-03-10 20:51:22 +0800

    2020-03-10

--- test.js
+++ test.js
@@ -1,12 +1,6 @@
-let obj = {
-    a: 1,
-    b: {
-      c: 2,
-      d: 3,
-    },
-    f:function(){console.log('a')}
-  }
-
-  let newObj = JSON.parse(JSON.stringify(obj))
-  obj.f()
-//   console.log(newObj.f())
+for (var index = 0; index < 5; index++) {
+  setTimeout(()=>{
+    console.log(i)
+  },i*1000)
+  
+}
