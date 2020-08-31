b82ce82e1d6e17ed52119b87d05564cdfabe6fec
Lucas <835934204@qq.com>
2020-08-31 20:34:21 +0800

    merge

--- test.js
+++ test.js
@@ -1,75 +1,26 @@
+var theThing = null;
+var count = 0;
+var result = 0;
 
-// demo02
-console.log('golb1');
+let replaceThing = function () {
+  count++;
+  let that = theThing;
+  let func = function () {
+    if (that) console.log("hi "+"boy!");
+  };
+  switch (count % 2) {
+    case 0:
+      var temp = 1;
+      result += temp;
+      break;
+    case 1:
+      theThing = {
+        longStr: new Array(1e8).join('*'),
+        someMethod: function () {
+          console.log("a");
+        }
+      }
+  };
+};
 
-setTimeout(function() {
-    console.log('timeout1');
-    process.nextTick(function() {
-        console.log('timeout1_nextTick');
-    })
-    new Promise(function(resolve) {
-        console.log('timeout1_promise');
-        resolve();
-    }).then(function() {
-        console.log('timeout1_then')
-    })
-})
-
-setImmediate(function() {
-    console.log('immediate1');
-    process.nextTick(function() {
-        console.log('immediate1_nextTick');
-    })
-    new Promise(function(resolve) {
-        console.log('immediate1_promise');
-        resolve();
-    }).then(function() {
-        console.log('immediate1_then')
-    })
-})
-
-process.nextTick(function() {
-    console.log('glob1_nextTick');
-})
-new Promise(function(resolve) {
-    console.log('glob1_promise');
-    resolve();
-}).then(function() {
-    console.log('glob1_then')
-})
-
-setTimeout(function() {
-    console.log('timeout2');
-    process.nextTick(function() {
-        console.log('timeout2_nextTick');
-    })
-    new Promise(function(resolve) {
-        console.log('timeout2_promise');
-        resolve();
-    }).then(function() {
-        console.log('timeout2_then')
-    })
-})
-
-process.nextTick(function() {
-    console.log('glob2_nextTick');
-})
-new Promise(function(resolve) {
-    console.log('glob2_promise');
-    resolve();
-}).then(function() {
-    console.log('glob2_then')
-})
-
-setImmediate(function() {
-    console.log('immediate2');
-    process.nextTick(function() {
-        console.log('immediate2_nextTick');
-    })
-    new Promise(function(resolve) {
-        console.log('immediate2_promise');
-        resolve();
-    }).then(function() {
-        console.log('immediate2_then')
-    })
-})
+const timer = setInterval(replaceThing, 100);
--- zt2.js
+++ zt2.js
@@ -1,7 +1,20 @@
-const array = 'aaaba'
+const list = [{a:1,b:11},{a:3,b:12},{a:2,b:15},{a:2,b:14},{a:3,b:15}]
+console.log(quicksort(list))
+function quicksort(array=[]){
+  if(array.length<=1) return array
+  
+  let middle = Math.floor((array.length)/2)
+  let base = array.splice(middle,1)[0]
 
-// 统计两次的
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+  let small =[],large=[]
+  for (let index = 0; index < array.length; index++) {
+    if(array[index].a <middle.a){
+      small.push({...array[index]})
+    }else{
+      large.push({...array[index]})
+    }
+  }
+
+  return quicksort(small).concat([base],quicksort(large))
 }
+
