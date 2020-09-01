27343a27e04747207569d72e2e4cba539f54bf41
lucaskslu(卢开书) <lucaskslu@tencent.com>
2020-09-01 21:34:39 +0800

    2020-09-01

--- zt2.js
+++ zt2.js
@@ -1,20 +1,15 @@
-const list = [{a:1,b:11},{a:3,b:12},{a:2,b:15},{a:2,b:14},{a:3,b:15}]
-console.log(quicksort(list))
-function quicksort(array=[]){
-  if(array.length<=1) return array
-  
-  let middle = Math.floor((array.length)/2)
-  let base = array.splice(middle,1)[0]
-
-  let small =[],large=[]
-  for (let index = 0; index < array.length; index++) {
-    if(array[index].a <middle.a){
-      small.push({...array[index]})
-    }else{
-      large.push({...array[index]})
+let Animal = {
+    leg: function() {
+        // todo
+        
     }
-  }
-
-  return quicksort(small).concat([base],quicksort(large))
 }
 
+
+let cat =  // todo
+let human = //todo
+
+// 要求输出
+cat.leg === human.leg // true
+cat.leg() // 4
+human.leg() // 2
